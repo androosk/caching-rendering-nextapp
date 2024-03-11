9a46cf92b839fef75897be097ae9ebac9eea1b7f
androosk <atirpok3@gmail.com>
2024-03-11 15:57:18 -0600

    a few ui tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>Insanity check</h1>
-      </div>
+    <main className="flex flex-grow flex-col items-center justify-center p-24">
+      <h1 className="text-4xl leading-10">
+        Server Side and Client Side Rendering
+      </h1>
+      <p className="text-4xl leading-10">
+        In Next JS 14, TypeScript and Tailwind
+      </p>
     </main>
   );
 }
